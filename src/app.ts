acd76aa15dea043c335c9792ddc85d2c361f25de
galibhub <ibrahimgalib00@gmail.com>
2026-07-18 16:28:00 +0600

    setup basic middleware and connect prisma

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,23 @@
 import express, { Application, Request, Response } from "express"
-
+import cors from "cors";
+import config from "./config";
+import cookieParser from "cookie-parser";
 
 const app : Application = express();
+
+//middleware
+app.use(
+  cors({
+    origin: config.app_url,
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
 
 //root route
 app.get("/",(req:Request, res: Response)=>{
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import app from "./app";
 import config from "./config";
+import { prisma } from "./lib/prisma";
 import "dotenv/config";
 
 const PORT = config.port;
@@ -7,12 +8,14 @@ const PORT = config.port;
 
 async function main() {
     try {
-        
+        await prisma.$connect();
+        console.log("Database connect Successfully")
         app.listen(PORT, ()=>{
             console.log(`server is running on port ${PORT}`)
         })
     } catch (error) {
         console.log("Error starting the server: ",error);
+        await prisma.$disconnect();
         process.exit(1);
     }
 }
